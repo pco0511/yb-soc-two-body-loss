f9014e899e5753b7c8d035da7f38dd459b01f27f
ChanwooPark <pco0511@gmail.com>
2025-03-31 03:16:13 +0900

    biorthogonal basis handled

--- scripts/main0.py
+++ scripts/main0.py
@@ -5,6 +5,7 @@ import itertools
 import functools
 import operator
 import pickle
+import json
 from pprint import pprint
 
 os.environ["XLA_PYTHON_CLIENT_ALLOCATOR"] = "platform"
@@ -20,14 +21,18 @@ import matplotlib.pyplot as plt
 
 from ybsoc import *
 
+save_eigenvectors = False
+
 simulation_name = "dissipative-8-4"
 data_dir = "/home/pco0511/yb-soc-two-body-loss/data"
 root_path = os.path.join(data_dir, simulation_name)
 
-metadata_path = os.path.join(root_path, "metadata.pkl")
-eigenvector_path = os.path.join(root_path, "eigenvectors")
+metadata_path = os.path.join(root_path, "metadata.json")
+lefteigenvector_path = os.path.join(root_path, "lefteigenvectors")
+righteigenvector_path = os.path.join(root_path, "righteigenvectors")
 eigenvalue_path = os.path.join(root_path, "eigenvalues")
 mics_path = os.path.join(root_path, "miscellaneous")
+log_path = os.path.join(root_path, "log")
 
 metadata = {
     "fixed": {
@@ -46,7 +51,10 @@ metadata = {
     },
 }
 
-os.makedirs(eigenvector_path, exist_ok=True)
+if save_eigenvectors:
+    os.makedirs(lefteigenvector_path, exist_ok=True)
+    os.makedirs(righteigenvector_path, exist_ok=True)
+    
 os.makedirs(eigenvalue_path, exist_ok=True)
 os.makedirs(mics_path, exist_ok=True)
 
@@ -59,17 +67,30 @@ min_E_r_sus = np.empty((data_len,))
 for idx, unfixed_params in tqdm.tqdm(enumerate(itertools.product(*metadata["unfixed"].values())), total=data_len):
     keys = metadata["unfixed"].keys()
     unfixed_params = dict(zip(keys, unfixed_params))
+    
+    # make a system
     system = YbSOC2bodyLoss(
         **metadata["fixed"],
         **unfixed_params,
         array_type='jax'
     )
+    
+    # construct dense matrix representation of hamiltonian
     hamiltonian = system.dense_hamiltonian()
-    eigenvalues, eigenvectors = jnp.linalg.eig(hamiltonian)
+    
+    # diagonalize and construct biorthogonal basis
+    eigenvalues, lefteigenvectors, righteigenvectors = jax.lax.linalg.eig(hamiltonian)
+    norm_factor = jnp.einsum('ij,ij->j', lefteigenvectors.conj(), righteigenvectors)
+    righteigenvectors /= norm_factor
+    # lefteigenvectors[: i], righteigenvectors[: j] are biorthogonal.
+    # assert jnp.allclose(jnp.einsum('ki,kj->ij', lefteigenvectors.conj(), righteigenvectors), jnp.eyes(system.dim))
+    
     # save_data
     np.save(os.path.join(eigenvalue_path, f"{idx}.npy"), np.array(eigenvalues))
-    np.save(os.path.join(eigenvector_path, f"{idx}.npy"), np.array(eigenvectors))
-
+    if save_eigenvectors:
+        np.save(os.path.join(lefteigenvector_path, f"{idx}.npy"), np.array(lefteigenvectors))
+        np.save(os.path.join(righteigenvector_path, f"{idx}.npy"), np.array(righteigenvectors))
+     
     E_r = jnp.real(eigenvalues)
     E_i = jnp.imag(eigenvalues)
 
@@ -79,21 +100,22 @@ for idx, unfixed_params in tqdm.tqdm(enumerate(itertools.product(*metadata["unfi
 
     indices = jnp.array([min_E_r_idx, min_E_i_idx, max_E_i_idx])
     
-    reduced_eigenvectors = eigenvectors[:, indices]
+    reduced_lefteigenvectors = lefteigenvectors[:, indices]
+    reduced_righteigenvectors = righteigenvectors[:, indices]
     
     suseptibility_matrix = system.pair_susceptibility_matrix()
-    suseptibility = jnp.einsum('ji,jk,ki->i', reduced_eigenvectors.conj(), suseptibility_matrix, reduced_eigenvectors)
+    suseptibility = jnp.einsum('ji,jk,ki->i', reduced_lefteigenvectors.conj(), suseptibility_matrix, reduced_righteigenvectors)
     
     min_E_r_sus[idx] = suseptibility[0].astype(jnp.float64)
     min_E_i_sus[idx] = suseptibility[1].astype(jnp.float64)
     max_E_i_sus[idx] = suseptibility[2].astype(jnp.float64)
     
-    del system, hamiltonian, eigenvalues, eigenvectors, reduced_eigenvectors, suseptibility_matrix, suseptibility, E_r, E_i
+    # free resources
+    del system, hamiltonian, eigenvalues, lefteigenvectors, righteigenvectors, reduced_lefteigenvectors, reduced_righteigenvectors, suseptibility_matrix, suseptibility, E_r, E_i
     gc.collect()
 
-
-with open(metadata_path, "wb") as f:
-    pickle.dump(metadata, f)
+with open(metadata_path, "w+") as f:
+    json.dump(metadata, f, indent=4)
 
 np.save(os.path.join(mics_path, "min_E_i_sus.npy"), min_E_i_sus)
 np.save(os.path.join(mics_path, "max_E_i_sus.npy"), max_E_i_sus)
