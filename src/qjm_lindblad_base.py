2213c6047efa9f904bcc1965cf8004ad32975d98
Your Name <you@example.com>
2025-09-26 19:42:06 +0000

    from vast

--- src/qjm_lindblad_base.py
+++ src/qjm_lindblad_base.py
@@ -44,7 +44,7 @@ parser.add_argument('--group_name', type=str, default="temp")
 parser.add_argument('--run_name', type=str, help='Root directory for data and figures')
 parser.add_argument('--nh', action='store_true', help='Use Non-Hermitian Hamiltonian instead of Lindblad operators')
 parser.add_argument('--initial_state', type=str, required=True, help='initial state for simulation.', choices=[
-    'aligned_up', 'mixture', 'superposition', 'soc_ground', 'custom'
+    'aligned_up', 'mixture', 'superposition', 'soc_ground', 'soc_lower', 'custom'
 ])
 parser.add_argument('--theta', type=float, default=0.0, help='parameter for super position: \\cos(\\theta/2)\\ket{0} + e^{i\\phi}\\sin(\\theta/2)\\ket{1}')
 parser.add_argument('--phi', type=float, default=0.0, help='parameter for super position: \\cos(\\theta/2)\\ket{0} + e^{i\\phi}\\sin(\\theta/2)\\ket{1}')
@@ -257,11 +257,11 @@ match (args.initial_state):
             )
             basis = hilb.get_momentum_eigenstate(basis_config)
             initial_state = initial_state + amp * basis
-    case "soc_ground":
-        
+    case "soc_ground" | "soc_lower":
         spectrum, lowlyings = ybsoc.manybody_spectrum(
             max_par_sub_hilb,
-            hbar, k_r, m_Yb, delta, omega_R
+            hbar, k_r, m_Yb, delta, omega_R,
+            lower_only=(args.initial_state == "soc_lower")
         )
         
         if zerotemp:
